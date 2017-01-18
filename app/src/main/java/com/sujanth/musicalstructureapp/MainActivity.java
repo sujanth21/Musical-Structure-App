2e9e217ff674d207dbf90611963b51d9af4c6b0a
Sujanth <sujanth1987@gmail.com>
2017-01-18 23:01:03 +1100

    Activities added

--- app/src/main/java/com/sujanth/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/sujanth/musicalstructureapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sujanth.musicalstructureapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button musicLibraryButton = (Button)findViewById(R.id.library_button);
+        musicLibraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent musicLibraryIntent = new Intent(MainActivity.this, MusicLibraryActivity.class);
+                startActivity(musicLibraryIntent);
+            }
+        });
+
+        Button artistButton = (Button) findViewById(R.id.artist_button);
+        artistButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent artistIntent = new Intent(MainActivity.this, ArtistActivity.class);
+                startActivity(artistIntent);
+            }
+        });
+
+        Button playlistButton = (Button) findViewById(R.id.playlist_button);
+        playlistButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent playlistIntent = new Intent(MainActivity.this, PlaylistActivity.class);
+                startActivity(playlistIntent);
+            }
+        });
+
+        final Button storeButton = (Button) findViewById(R.id.store_button);
+        storeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent storeIntent = new Intent(MainActivity.this, StoreActivity.class);
+                startActivity(storeIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sujanth/musicalstructureapp/MusicLibraryActivity.java
@@ -0,0 +1,13 @@
+package com.sujanth.musicalstructureapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicLibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_library);
+    }
+}
